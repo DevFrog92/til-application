24f59814fb818c44ff35ee46e9f8312deb172929
DevFrog92 <yongsu1481@gmail.com>
2022-07-03 02:12:08 +0900

    ✨ Implement axios interceptors

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,33 @@
 import axios from 'axios';
+import { setInterceptors } from '@/api/common/interceptors';
 
 // endpoint: http://localhost:3000/
 
 // VUE_APP_가 붙은 변수는 자동으로 로드한다,
 
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-});
+function createInstance() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  });
 
+  return setInterceptors(instance);
+}
+
+const instance = createInstance();
+
+// 회원가입 api
 function registerUser(userData) {
-  return instance.post('/signup', userData);
+  return instance.post('signup', userData);
 }
 
+// 로그인 api
 function loginUser(userData) {
-  return instance.post('/login', userData);
+  return instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+// 학습 노크 데이터를 조회하는 api
+function fetchPosts() {
+  return instance.get('posts');
+}
+
+export { registerUser, loginUser, fetchPosts };
